9d97bb4fac0d1aff88f7326e6edac08d44170b1b
Andrey <sanin.213@gmail.com>
2022-12-12 21:38:32 +0300

    task 1.2 added another solution

--- seminar/lesson1/task2.py
+++ seminar/lesson1/task2.py
@@ -9,3 +9,13 @@ num3 = int(input("Введите 3 число: "))
 num4 = int(input("Введите 4 число: "))
 num5 = int(input("Введите 5 число: "))
 print(f"Максимум равен {max(num1, num2, num3, num4, num5)}")
+
+#Другое решение
+
+my_max = 0
+for _ in range(5):
+    num = int(input('Введите число: '))
+    if my_max < num:
+        my_max = num
+
+print(my_max)
